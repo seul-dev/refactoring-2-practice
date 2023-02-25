00c6c01b900c64574fb23945fa38df284c6ad409
seul-dev <seul.dev@gmail.com>
2023-02-25 22:58:17 +0900

    solve 7-9(알고리즘 교체하기)

--- ch7/7-9.js
+++ ch7/7-9.js
@@ -1,16 +1,6 @@
 function foundPerson(people) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-  return '';
+  const candidates = ['Don', 'John', 'Kent'];
+  return people.find((person) => candidates.includes(person)) || '';
 }
 
 console.log(foundPerson(['John']));
