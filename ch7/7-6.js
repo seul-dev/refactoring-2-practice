445f4139fc3429a6c3e9b57c2b4d5a33b112b613
seul-dev <seul.dev@gmail.com>
2023-02-25 22:34:45 +0900

    solve 7-6(클래스 인라인하기)

--- ch7/7-6.js
+++ ch7/7-6.js
@@ -1,11 +1,10 @@
-export class TrackingInformation {
+export class Shipment {
   #shippingCompany;
   #trackingNumber;
   constructor(trackingNumber, shippingCompany) {
     this.#trackingNumber = trackingNumber;
     this.#shippingCompany = shippingCompany;
   }
-
   get shippingCompany() {
     return this.#shippingCompany;
   }
@@ -22,32 +21,13 @@ export class TrackingInformation {
     this.#trackingNumber = arg;
   }
 
-  get display() {
+  get trackingInfo() {
     return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
 
-export class Shipment {
-  #trackingInformation;
-  constructor(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
-
-  get trackingInfo() {
-    return this.#trackingInformation.display;
-  }
-
-  get trackingInformation() {
-    return this.#trackingInformation;
-  }
-
-  set trackingInformation(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
-}
-
-const shipment = new Shipment(new TrackingInformation(999, 'Maersk'));
+const shipment = new Shipment(999, 'Maersk');
 console.log(shipment.trackingInfo);
 
-shipment.trackingInformation.shippingCompany = 'COSCO';
+shipment.shippingCompany = 'COSCO';
 console.log(shipment.trackingInfo);
