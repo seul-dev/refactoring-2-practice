bed50abc602a6b72bf86397a4ee7d4217fc59627
seul-dev <seul.dev@gmail.com>
2023-02-25 22:24:26 +0900

    solve 7-5(클래스 추출하기)

--- ch7/7-5.js
+++ ch7/7-5.js
@@ -1,11 +1,33 @@
+class TelephoneNumber {
+  #areaCode;
+  #number;
+  constructor(areaCode, number) {
+    this.#areaCode = areaCode;
+    this.#number = number;
+  }
+  get areaCode() {
+    return this.#areaCode;
+  }
+  set areaCode(arg) {
+    this.#areaCode = arg;
+  }
+
+  get number() {
+    return this.#number;
+  }
+  set number(arg) {
+    this.#number = arg;
+  }
+  get toString() {
+    return `(${this.#areaCode}) ${this.#number}`;
+  }
+}
 class Person {
   #name;
-  #officeAreaCode;
-  #officeNumber;
-  constructor(name, areaCode, number) {
+  #telephoneNumber;
+  constructor(name, telephone) {
     this.#name = name;
-    this.#officeAreaCode = areaCode;
-    this.#officeNumber = number;
+    this.#telephoneNumber = telephone;
   }
 
   get name() {
@@ -17,27 +39,27 @@ class Person {
   }
 
   get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+    return this.#telephoneNumber.toString;
   }
 
   get officeAreaCode() {
-    return this.#officeAreaCode;
+    return this.#telephoneNumber.areaCode;
   }
 
   set officeAreaCode(arg) {
-    this.#officeAreaCode = arg;
+    this.#telephoneNumber.officeAreaCode = arg;
   }
 
   get officeNumber() {
-    return this.#officeNumber;
+    return this.#telephoneNumber.number;
   }
 
   set officeNumber(arg) {
-    this.#officeNumber = arg;
+    this.#telephoneNumber.number = arg;
   }
 }
 
-const person = new Person('엘리', '010', '12345678');
+const person = new Person('엘리', new TelephoneNumber('010', '12345678'));
 console.log(person.name);
 console.log(person.officeAreaCode);
 console.log(person.officeNumber);
