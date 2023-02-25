74bce9ec8aa53c4aa16d68dbc16ccc06710396b2
seul-dev <seul.dev@gmail.com>
2023-02-25 15:21:08 +0900

    solve 6-5, 6-7(변수 이름짓기, 함수 선언 바꾸기)

--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,4 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+export function inNewEngland(state) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(state);
 }
+// 함수 안에서 필요한 데이터만 받아온다.(재사용성⤴)
