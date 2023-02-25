81160d22face8ec4281b59cbf88ccd6412800dac
seul-dev <seul.dev@gmail.com>
2023-02-25 16:30:16 +0900

    solve 6-6, 6-9, 6-10(캡슐화)

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,10 +1,21 @@
+import _ from 'lodash';
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
 }
 
-export function baseRate(month, year) {
-  if (year === 2017 && month === 5) return 0.1;
-  return 0.2;
+export function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  return result;
+}
+
+function baseRate(month, year) {
+  return year === 2017 && month === 5 ? 0.1 : 0.2;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
 }
--- ch6/6-9.js
+++ ch6/6-9.js
@@ -1,10 +1,47 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+class Reading {
+  #cutomer;
+  #quantity;
+  #month;
+  #year;
+  constructor(data) {
+    this.#cutomer = data.customer;
+    this.#quantity = data.quantity;
+    this.#month = data.month;
+    this.#year = data.year;
+  }
+  get customer() {
+    return this.#cutomer;
+  }
+  get quantity() {
+    return this.#quantity;
+  }
+  get month() {
+    return this.#month;
+  }
+  get year() {
+    return this.#year;
+  }
+  get baseRate() {
+    return this.#year === 2017 && this.#month === 5 ? 0.1 : 0.2;
+  }
+  get baseCharge() {
+    return this.baseRate * this.quantity;
+  }
+  get taxThreshold() {
+    return 0.1;
+  }
+  get texableCharge() {
+    return Math.max(0, this.baseCharge - this.taxThreshold);
+  }
+}
+
+const reading = new Reading({
+  customer: 'ivan',
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
 
 export function acquireReading() {
   return reading;
-}
-
-export function baseRate(month, year) {
-  if (year === 2017 && month === 5) return 0.1;
-  return 0.2;
 }
