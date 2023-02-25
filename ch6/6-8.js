6ce1cfe42fad3ce4b6146e9781b48c8dd8919ea6
seul-dev <seul.dev@gmail.com>
2023-02-25 15:22:28 +0900

    solve 6-8(매개변수 객체만들기)

--- ch6/6-8.js
+++ ch6/6-8.js
@@ -1,5 +1,5 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter((r) => !range.contanins(r.temp));
 }
 
 const station = {
@@ -12,13 +12,26 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+class NumberRange {
+  #min;
+  #max;
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+  get min() {
+    return this.#min;
+  }
+  get max() {
+    return this.#max;
+  }
+  contanins(number) {
+    return number >= this.#min && number <= this.#max;
+  }
+}
+
+const operationPlan = new NumberRange(51, 53);
+
+const result = readingsOutsideRange(station, operationPlan);
+console.log(result);
