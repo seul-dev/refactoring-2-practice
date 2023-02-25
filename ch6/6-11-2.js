53b464698a4a51782250fbf42adb9450a3d9b327
seul-dev <seul.dev@gmail.com>
2023-02-25 18:00:50 +0900

    solution-1 6-11-2 단계쪼개기

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,35 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+run(process.argv);
+
+function run(args) {
+  const command = parseCommand(args);
+  return countOrders(command);
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function parseCommand(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  return {
+    fileName,
+    countReadyOnly: process.argv.includes('-r'),
+  };
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countOrders(command) {
+  const rawData = fs.readFileSync(command.fileName);
+  const orders = JSON.parse(rawData);
+
+  if (command.countReadyOnly) {
+    console.log(orders.filter((order) => order.status === 'ready').length);
+  } else {
+    console.log(orders.length);
+  }
 }
