00efb69e97eaa7fa596beef808eb880013cb4dcd
seul-dev <seul.dev@gmail.com>
2023-02-26 16:43:05 +0900

    solve 8-8(반복문을 파이프라인으로 바꾸기, 죽은코드 제거하기)

--- ch8/8-8.js
+++ ch8/8-8.js
@@ -1,26 +1,13 @@
 export function acquireData(input) {
   const lines = input.split('\n');
-  let firstLine = true;
-  const result = [];
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
-      result.push({ city: record[0].trim(), phone: record[2].trim() });
-    }
-  }
-  return result;
-}
 
-const input = `office, country, telephone\n
-Chicago, USA, +1 312 373 1000\n
-Beijing, China, +86 4008 900 505\n
-Bangalore, India, +91 80 4064 9570\n
-Porto Alegre, Brazil, +55 51 3079 3550\n
-Chennai, India, +91 44 660 44766`;
-const result = acquireData(input);
-console.log(result);
+  return lines
+    .slice(1)
+    .filter((line) => line.trim() !== '')
+    .map((line) => line.split(','))
+    .filter((record) => record[1].trim() === 'India')
+    .map((record) => ({
+      city: record[1].trim(),
+      phone: record[2].trim(),
+    }));
+}
