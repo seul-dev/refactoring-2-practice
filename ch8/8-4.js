a8e80f72fdfcb62eab81a21c772821c8f174a024
seul-dev <seul.dev@gmail.com>
2023-02-26 15:24:39 +0900

    solve 8-4(문장을 호출한 곳으로 옮기기)

--- ch8/8-4.js
+++ ch8/8-4.js
@@ -2,14 +2,16 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>location: ${photo.location}</p>\n`); //
 }
 
 function listRecentPhotos(outStream, photos) {
   photos
-    .filter((p) => p.date > recentDateCutoff())
-    .forEach((p) => {
+    .filter((photo) => photo.date > recentDateCutoff())
+    .forEach((photo) => {
       outStream.write('<div>\n');
-      emitPhotoData(outStream, p);
+      emitPhotoData(outStream, photo);
+      outStream.write(`<p>위치: ${photo.location}</p>\n`); //
       outStream.write('</div>\n');
     });
 }
@@ -17,7 +19,6 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
