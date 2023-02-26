1c5f507d5fa055e72fe10603e3eeb0fb2dff978d
seul-dev <seul.dev@gmail.com>
2023-02-26 15:15:33 +0900

    solve 8-2(필드 옮기기)

--- ch8/8-2-1.js
+++ ch8/8-2-1.js
@@ -1,24 +1,19 @@
+/** 필드를 근접한 곳으로 옮기기 */
 export class Customer {
   #name;
   #discountRate;
-  #contract;
   constructor(name, discountRate) {
     this.#name = name;
-    this.#discountRate = discountRate;
-    this.#contract = new CustomerContract(this.dateToday());
-  }
-
-  get discountRate() {
-    return this.#discountRate;
+    this.#contract = new CustomerContract(this.dateToday(), discountRate);
   }
 
   becomePreferred() {
-    this.#discountRate += 0.03;
+    this.#contract.discountRate += 0.03;
     // 다른 코드들이 있음...
   }
 
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
+    return amount.subtract(amount.multiply(this.#contract.discountRate));
   }
 
   dateToday() {
@@ -28,7 +23,16 @@ export class Customer {
 
 class CustomerContract {
   #startDate;
-  constructor(startDate) {
+  #contract;
+  constructor(startDate, discountRate) {
     this.#startDate = startDate;
+    this.#discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this.#discountRate;
+  }
+  set discountRate(value) {
+    this.#discountRate = value;
   }
 }
