023d140fc150978614e7478141492f3b4cc4d825
seul-dev <seul.dev@gmail.com>
2023-03-02 12:17:45 +0900

    solve 10-4 조건부 로직을 다형성으로 바꾸기

--- ch10/10-4-1.js
+++ ch10/10-4-1.js
@@ -1,32 +1,48 @@
 export function plumages(birds) {
-  let map = birds.map((b) => [b.name, plumage(b)]);
+  let map = birds.map((b) => [b.name, b.plumage]);
   let map1 = new Map(map);
   return map1;
 }
 export function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+  return new Map(birds.map((b) => [b.name, b.airSpeedVelocity]));
 }
-export function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
+
+class Bird {
+  #name;
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+  get plumage() {
+    return 'unknown';
+  }
+  get airSpeedVelocity() {
+    return null;
   }
 }
-export function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
+
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return 'average';
+  }
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+  }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? 'scorched' : 'beautiful';
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + bird.voltage / 10;
   }
 }
