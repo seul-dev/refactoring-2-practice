f5d5198ebd21266bc0df98ff137dd59199944f85
seul-dev <seul.dev@gmail.com>
2023-03-02 12:49:05 +0900

    null 객체로 특이케이스 처리 예시코드

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -54,3 +54,43 @@ export function customerName(site) {
 
   return aCustomer.name;
 }
+
+class Hotel {
+  constructor() {
+    this.rooms = [];
+  }
+
+  addRoom(roomNumber) {
+    this.rooms[roomNumber] = new Room(roomNumber);
+  }
+
+  emptyRoom(roomNumber) {
+    this.rooms[roomNumber] = new EmptyRoom(roomNumber);
+  }
+
+  cleanRooms() {
+    this.rooms.forEach((room) => room.clean());
+  }
+}
+
+class Room {
+  constructor(roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  clean() {
+    console.log(`${this.roomNumber}번 방을 청소합니다.`);
+  }
+}
+
+class EmptyRoom extends Room {
+  clean() {
+    console.log(`${this.roomNumber}번 방은 빈 방입니다.`);
+  }
+}
+
+const hotel = new Hotel();
+hotel.addRoom(0);
+hotel.addRoom(1);
+hotel.emptyRoom(1);
+hotel.cleanRooms();
