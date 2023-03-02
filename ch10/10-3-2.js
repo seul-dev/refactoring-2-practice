0a4da0b8e1295a836227ab9916980ff9f41a0c49
seul-dev <seul.dev@gmail.com>
2023-03-02 09:11:00 +0900

    solve 10-3 중첩 조건문을 보호구문으로 바꾸기

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,16 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (!isEligibleForAdjusCapital(instrument)) {
+    return 0;
   }
-  return result;
+  return (
+    (instrument.income / instrument.duration) * anInstrument.adjustmentFactor
+  );
+}
+
+function isEligibleForAdjusCapital(instrument) {
+  return (
+    instrument.capital > 0 &&
+    instrument.interestRate > 0 &&
+    instrument.duration > 0
+  );
 }
