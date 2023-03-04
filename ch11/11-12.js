56dc9d72297ea2dc0f0238a4dc77410015c6a039
seul-dev <seul.dev@gmail.com>
2023-03-04 22:51:28 +0900

    오류 코드를 예외로 바꾸기

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,23 @@
 function localShippingRules(data) {
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23);
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super(errorCode);
+    this.code = errorCode;
+  }
+  get name() {
+    return 'OrderProcessingError';
+  }
+}
+
+//사용할때
+try {
+  localShippingRules();
+} catch (error) {
+  if (error instanceof OrderProcessingError) {
+    console.log(error);
+  }
 }
