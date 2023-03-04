653e43485504db4224e2132ae40fbfb8ff9de33b
seul-dev <seul.dev@gmail.com>
2023-03-04 22:58:51 +0900

    예외를 사전 확인으로 바꾸기

--- ch11/11-13.js
+++ ch11/11-13.js
@@ -1,14 +1,7 @@
+// 예외와 예상되는 실패를 구분하는 것이 좋다
 const values = [];
 function getValueForPeriod(periodNumber) {
-  const value = values[periodNumber];
-  if (!value) {
-    throw new Error('value is undefined');
-  }
-  return value;
+  return values[periodNumber] || 0;
 }
 
-try {
-  getValueForPeriod(-10);
-} catch (error) {
-  console.log('에러 발생!');
-}
+getValueForPeriod(-10);
