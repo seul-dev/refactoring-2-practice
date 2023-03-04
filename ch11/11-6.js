8bd562fc6fee35c7cffb70a480985c050ace2040
seul-dev <seul.dev@gmail.com>
2023-03-04 16:55:18 +0900

    질의함수를 매개변수로 바꾸기

--- ch11/11-6.js
+++ ch11/11-6.js
@@ -1,7 +1,8 @@
-targetTemperature(aPlan);
+targetTemperature(aPlan, thermostat.currentTemperature);
 
 // 다른모듈에 있는 함수라고 가정
-function targetTemperature(aPlan) {
-  currentTemperature = thermostat.currentTemperature;
+function targetTemperature(plan, currentTemperature) {
+  currentTemperature = currentTemperature;
   // ...
 }
+// 응집도가 떨어진 외무 모듈간에 데이터를 주고 받을때에는 필요한 데이터를 전달받는 매개변수로
