0a86af7e679441a866073b4421f5920c0d101cde
seul-dev <seul.dev@gmail.com>
2023-03-04 20:56:09 +0900

    함수를 명령으로 바꾸기

--- ch11/11-9.js
+++ ch11/11-9.js
@@ -1,22 +1,38 @@
 export function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
-
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
-  }
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
-  }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
+  return new Scorer(candidate, medicalExam, scoringGuide).excute();
 }
 
+class Scorer {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this.candidate = candidate;
+    this.medicalExam = medicalExam;
+    this.scoringGuide = scoringGuide;
+  }
+
+  excute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this.medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = 'regular';
+    if (
+      this.scoringGuide.stateWithLowCertification(this.candidate.originState)
+    ) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
+  }
+}
+
+// 명령 패턴(Command Pattern)은 명령을 추상화 해서 객체로 다루는 디자인 패턴
+// 일급함수를 지원하지 않는 프로그래밍언어에서 유용하게 쓰일 수 있다.
 export class ScoringGuide {
   stateWithLowCertification(state) {
     return state < 5;
