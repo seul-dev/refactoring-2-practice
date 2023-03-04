710f6ad9e40894ce348474e69fa5f75c6dc0c963
seul-dev <seul.dev@gmail.com>
2023-03-04 16:14:14 +0900

    플래그 인수 제거하기

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -1,19 +1,21 @@
+//플래그 인수란 호출되는 함수가 실행할 로직을 호출하는 쪽에서 선택하기 위해 전달하는 인수
 // 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+function setWidth(value) {
+  this._width = value
+} 
+
+function setHeight (value) {
+  this._height = value
 }
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(curstom) {}
+  premiumBook(curstom) {}
+
+  #book(customer, isPremium) {}
 }
 
 // 예제 3
-function setSwitch(on);
+function setSwitchOn();
+function setSwitchOff();
