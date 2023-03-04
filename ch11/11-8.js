0daad3cb46491718b4aeae698ee7ddf1438f8124
seul-dev <seul.dev@gmail.com>
2023-03-04 18:07:56 +0900

    생성자를 팩토리 함수로 바꾸기

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,3 +1,4 @@
+// 인스턴스를 만드는 로직을 캡슐화하기 위해 (constructor를 private으로 해야 함)
 export class Employee {
   constructor(name, typeCode) {
     this._name = name;
@@ -14,4 +15,18 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E');
+  }
+
+  static createManager(name) {
+    return new Employee(name, 'M');
+  }
+
+  static createSalesman(name) {
+    return new Employee(name, 'S');
+  }
 }
+
+const engineer = Employee.createEngineer('seul');
