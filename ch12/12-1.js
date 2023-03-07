c07ee37e8a51af9eee668a6a1a17c173ee25dd20
seul-dev <seul.dev@gmail.com>
2023-03-07 19:19:08 +0900

    메서드 끌어올리기

--- ch12/12-1.js
+++ ch12/12-1.js
@@ -1,24 +1,21 @@
 // 예시 1
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   get name() {}
 }
 
-class Engineer extends Employee {
-  get name() {}
-}
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
+class Party {
   get totalAnnualCost() {
     return this.monthlyCost * 12;
   }
 }
-class Employee extends Party {
-  get annualCost() {
-    return this.monthlyCost * 12;
-  }
-}
+
+class Department extends Party {}
+class Employee extends Party {}
+
+// 서브 클래스에서 공통적으로 사용하는 메서드를 상위클래스로 끌어올리기
+// 다른 로직이 필요하면 서브 클래스에서 메서드를 오버라이드하면 된다.
