7109d9199ba6c786e7790a1da0d531116bfaf9d3
seul-dev <seul.dev@gmail.com>
2023-03-07 19:27:59 +0900

    메서드 내리기, 필드 내리기

--- ch12/12-4.js
+++ ch12/12-4.js
@@ -1,6 +1,8 @@
-class Employee {
+class Employee {}
+
+class Engineer extends Employee {}
+class Salesperson extends Employee {
   get quota() {}
 }
 
-class Engineer extends Employee {}
-class Salesperson extends Employee {}
+// 특정 서브 클래스(하나 혹은 소수)와만 관련된 메서드는 슈퍼 클래스에서 제거하고 해당 서브클래스에 추가
