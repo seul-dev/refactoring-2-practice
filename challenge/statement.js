ca558e1e899db687f7600d8cfd050e4f735f3f5b
seul-dev <seul.dev@gmail.com>
2023-03-10 14:58:23 +0900

    함수 분리

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,48 +1,65 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
+  const volumeCredits = calcVolumeCredits();
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy': // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    const thisAmount = amountFor(playFor(perf), perf);
 
     // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${usdFormat(thisAmount)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usdFormat(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+
+  function playFor(performance) {
+    return plays[performance.playID];
+  }
+
+  function calcVolumeCredits() {
+    let result = 0;
+    for (let performance of invoice.performances) {
+      result += Math.max(performance.audience - 30, 0);
+
+      if ('comedy' === playFor(performance).type) {
+        result += Math.floor(performance.audience / 5);
+      }
+    }
+    return result;
+  }
+}
+
+function usdFormat(arg) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(arg / 100);
+}
+
+function amountFor(play, performance) {
+  let result = 0;
+  switch (play.type) {
+    case 'tragedy': // 비극
+      result = 40000;
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
+      }
+      break;
+    case 'comedy': // 희극
+      result = 30000;
+      if (performance.audience > 20) {
+        result += 10000 + 500 * (performance.audience - 20);
+      }
+      result += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
   return result;
 }
 
